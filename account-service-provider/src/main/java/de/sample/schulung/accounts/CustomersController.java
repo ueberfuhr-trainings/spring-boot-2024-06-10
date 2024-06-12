07c33291a4026b86f4d3aee9498373398c8c241d
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-06-12 06:23:47 +0200

    Introduce Swagger Codegen

--- account-service-provider/src/main/java/de/sample/schulung/accounts/CustomersController.java
+++ account-service-provider/src/main/java/de/sample/schulung/accounts/CustomersController.java
@@ -1,112 +1,94 @@
 package de.sample.schulung.accounts;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import de.sample.schulung.accounts.boundary.api.CustomersApi;
+import de.sample.schulung.accounts.boundary.api.model.Customer;
+import de.sample.schulung.accounts.boundary.api.model.CustomerState;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Stream;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-@RequestMapping("/api/v1/customers")
-public class CustomersController {
+@RequestMapping("/api/v1")
+public class CustomersController implements CustomersApi {
 
   private final Map<UUID, Customer> customers = new HashMap<>();
 
   {
-    this.createCustomer(
-      new Customer(
-        null,
-        "Max",
-        LocalDate.of(2010, Month.FEBRUARY, 10),
-        "active"
-      )
+    this.customersPost(
+      new Customer()
+        .name("Max")
+        .birthdate(LocalDate.of(2010, Month.FEBRUARY, 10))
+        .state(CustomerState.ACTIVE)
     );
-    this.createCustomer(
-      new Customer(
-        UUID.randomUUID(),
-        "Julia",
-        LocalDate.of(2011, Month.APRIL, 2),
-        "disabled"
-      )
+    this.customersPost(
+      new Customer()
+        .name("Julia")
+        .birthdate(LocalDate.of(2011, Month.APRIL, 2))
+        .state(CustomerState.DISABLED)
     );
   }
 
-  @GetMapping(
-    produces = MediaType.APPLICATION_JSON_VALUE)
-  Stream<Customer> getCustomers(
-    @RequestParam(value = "state", required = false)
-    String stateFilter
-  ) {
-    return this.customers
+  @Override
+  public ResponseEntity<List<Customer>> customersGet(CustomerState state) {
+    return ResponseEntity.ok(
+      this.customers
       .values()
       .stream()
-      .filter(customer -> stateFilter == null || stateFilter.equals(customer.getState()));
+      .filter(customer -> state == null || state == customer.getState())
+        .toList()
+    );
   }
 
-  @PostMapping(
-    consumes = MediaType.APPLICATION_JSON_VALUE,
-    produces = MediaType.APPLICATION_JSON_VALUE
-  )
-  ResponseEntity<Customer> createCustomer(
-    @RequestBody Customer customer
-  ) {
+  @Override
+  public ResponseEntity<Customer> customersPost(Customer customer) {
     var uuid = UUID.randomUUID();
     customer.setUuid(uuid);
     this.customers.put(uuid, customer);
     var uri = linkTo(
-      methodOn(CustomersController.class)
-        .findCustomerById(uuid)
+      methodOn(CustomersApi.class)
+        .customersIdGet(uuid)
     ).toUri();
     return ResponseEntity
       .created(uri)
       .body(customer);
   }
 
-  @GetMapping(
-    value = "/{uuid}",
-    produces = MediaType.APPLICATION_JSON_VALUE
-  )
-  Customer findCustomerById(
-    @PathVariable UUID uuid
-  ) {
+  @Override
+  public ResponseEntity<Customer> customersIdGet(UUID uuid) {
     var result = this.customers
       .get(uuid);
     if(result == null) {
       throw new NotFoundException();
     }
-    return result;
+    return ResponseEntity.ok(result);
   }
 
-  @PutMapping(
-    value = "/{uuid}",
-    consumes = MediaType.APPLICATION_JSON_VALUE
-  )
-  @ResponseStatus(HttpStatus.NO_CONTENT)
-  void replaceCustomer(
-    @PathVariable UUID uuid,
-    @RequestBody Customer customer) {
-    if (this.customers.containsKey(uuid)) {
-      customer.setUuid(uuid);
-      this.customers.put(uuid, customer);
+  @Override
+  public ResponseEntity<Void> customersIdPut(UUID uuid, Customer customer) {
+    if (!this.customers.containsKey(uuid)) {
+      throw new NotFoundException();
     }
-
+    customer.setUuid(uuid);
+    this.customers.put(uuid, customer);
+    return ResponseEntity.noContent().build();
   }
 
-  @DeleteMapping("/{uuid}")
-  @ResponseStatus(HttpStatus.NO_CONTENT)
-  void deleteCustomer(
-    @PathVariable UUID uuid
-  ) {
-    this.customers.remove(uuid);
+  @Override
+  public ResponseEntity<Void> customersIdDelete(UUID uuid) {
+    if(this.customers.remove(uuid) != null) {
+      throw new NotFoundException();
+    }
+    return ResponseEntity.noContent().build();
   }
 
 }
